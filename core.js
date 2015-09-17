45f7b99f49e817f8e7804011fb76b70681003b5e
galvaniccoffee <mail@jannisbecker.de>
2015-09-17 18:48:11 +0200

    Added experimental map border and new texture

--- core.js
+++ core.js
@@ -4,7 +4,9 @@
 
 var canvas, ctx, midP,
 tiles, sprites,
-MAP_WIDTH = 64, MAP_HEIGHT = 64, GROUND_TYPES = {grass: 0, stone: 1}, BUILDING_TYPES = {emtpy: 0, forest: 1, pylon: 2};
+MAP_WIDTH = 64, MAP_HEIGHT = 64, BORDER_THICKNESS = 5,
+GROUND_TYPES = {grass: 0, stone: 1, darkStone: 2},
+BUILDING_TYPES = {emtpy: 0, forest: 1, pylon: 2};
 
 window.onload = function () {
 	//Stuff
@@ -29,12 +31,17 @@ window.onload = function () {
     render.setup();
 
 
-    //Fill tiles with grass:
+    
     tiles = [];
+
+    //Test World Gen:
     for(var x = 0; x < MAP_WIDTH; x++) {
         tiles.push([]);
         for(var y = 0; y < MAP_HEIGHT; y++) {
-            tiles[x].push({ground: (Math.random() > 0.15 ? GROUND_TYPES.grass : GROUND_TYPES.stone), building: {type: (Math.random() > 0.50 ? BUILDING_TYPES.empty : BUILDING_TYPES.forest), data: {}}});
+            tiles[x].push({
+                ground: (Math.random() * MAP_WIDTH > x ?  GROUND_TYPES.darkStone : (Math.random() > 0.15 ? GROUND_TYPES.grass : GROUND_TYPES.stone)),
+                building: {type: (Math.random() > 0.50 ? BUILDING_TYPES.empty : BUILDING_TYPES.forest), data: {}}
+            });
         }
     }
 
@@ -54,78 +61,6 @@ function tick() {
     requestAnimationFrame(tick);
 }
 
-
-
-
-function rgbaStr(color) {
-    var nR, nG, nB, nA;
-    nR = Math.round(color.r);
-    nG = Math.round(color.g);
-    nB = Math.round(color.b);
-    nA = color.a;
-    return "rgba(" + String(nR) + ", " + String(nG) + ", " + String(nB) + ", "  + String(nA) + ")";
-}
-
-
-// By http://axonflux.com/handy-rgb-to-hsl-and-rgb-to-hsv-color-model-c
-/**
- * Converts an HSL color value to RGB. Conversion formula
- * adapted from http://en.wikipedia.org/wiki/HSL_color_space.
- * Assumes h, s, and l are contained in the set [0, 1] and
- * returns r, g, and b in the set [0, 255].
- *
- * @param   Number  h       The hue
- * @param   Number  s       The saturation
- * @param   Number  l       The lightness
- * @return  Array           The RGB representation
- */
-function hslaToRgb(h, s, l, a){
-    var r, g, b;
-
-    if(s == 0){
-        r = g = b = l; // achromatic
-    }else{
-        function hue2rgb(p, q, t){
-            if(t < 0) t += 1;
-            if(t > 1) t -= 1;
-            if(t < 1/6) return p + (q - p) * 6 * t;
-            if(t < 1/2) return q;
-            if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
-            return p;
-        }
-
-        var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-        var p = 2 * l - q;
-        r = hue2rgb(p, q, h + 1/3);
-        g = hue2rgb(p, q, h);
-        b = hue2rgb(p, q, h - 1/3);
-    }
-
-    return {r: Math.round(r * 255), g: Math.round(g * 255), b: Math.round(b * 255), a: a};
-}
-
-function cloneColor(color) {
-    return {r: color.r, g: color.g, b: color.b, a: color.a};
-}
-
-function colorVar(color, d) {
-    function rand(d) {
-        return d - (Math.random() * 2 * d);
-    }
-    return {r: color.r + rand(d), g: color.g + rand(d), b: color.b + rand(d), a: color.a};
-}
-
-function mixColors(col1, col2, pos) {
-    //Returns the color that's a mix between the 2 given ones, with position being the ratio between them
-    var nR, nG, nB, nA;
-    nR = Math.round(col1.r * (1 - pos) + col2.r * pos);
-    nG = Math.round(col1.g * (1 - pos) + col2.g * pos);
-    nB = Math.round(col1.b * (1 - pos) + col2.b * pos);
-    nA = col1.a * (1 - pos) + col2.a * pos;
-
-    return {col: {r: nR, g: nG, b: nB, a: nA},
-            string: "rgba(" + String(nR) + ", " + String(nG) + ", " + String(nB) + ", "  + String(nA) + ")"};
-}
 
 
 function sign(x) { return x > 0 ? 1 : x < 0 ? -1 : 0; }
--- render.js
+++ render.js
@@ -45,6 +45,11 @@ render.draw = function() {
     			sy = 0;
     			scol = "gray"; //TODO!
     		break;
+            case GROUND_TYPES.darkStone:
+                sx = 3;
+                sy = 0;
+                scol = "gray"; //TODO!
+            break;
     	}
     	if (camera.zoom < 0.5) {//TODO!
     		ctx.fillStyle = scol;
@@ -70,7 +75,15 @@ render.draw = function() {
     	if(tile.building.type != BUILDING_TYPES.empty) {//TODO! (?)
     		ctx.drawImage(TILE_SHEET, sx * TILE_SIZE, sy * TILE_SIZE,TILE_SIZE,TILE_SIZE,x * TILE_SIZE * camera.zoom - camera.offset.x,y * TILE_SIZE * camera.zoom - camera.offset.y,TILE_SIZE*camera.zoom,TILE_SIZE*camera.zoom);
     	}
-    	
+
+        //Map-Border:
+        //Just experimental. TODO!
+        if(x < BORDER_THICKNESS || x > MAP_WIDTH - BORDER_THICKNESS || y < BORDER_THICKNESS || y > MAP_HEIGHT - BORDER_THICKNESS) {
+            var off = Math.min(x, MAP_WIDTH - x, y, MAP_HEIGHT - y);
+            ctx.fillStyle = "rgba(0, 0, 0, " + (0.7 - 0.4 * (off / BORDER_THICKNESS)) + ")";
+            ctx.fillRect(Math.round(x * TILE_SIZE * camera.zoom - camera.offset.x), Math.round(y * TILE_SIZE * camera.zoom - camera.offset.y), Math.round(TILE_SIZE*camera.zoom), Math.round(TILE_SIZE*camera.zoom));
+    	}
+
     }
 
 	this.clear();
@@ -92,3 +105,73 @@ render.draw = function() {
         }
     }
 };
+
+function rgbaStr(color) {
+    var nR, nG, nB, nA;
+    nR = Math.round(color.r);
+    nG = Math.round(color.g);
+    nB = Math.round(color.b);
+    nA = color.a;
+    return "rgba(" + String(nR) + ", " + String(nG) + ", " + String(nB) + ", "  + String(nA) + ")";
+}
+
+
+// By http://axonflux.com/handy-rgb-to-hsl-and-rgb-to-hsv-color-model-c
+/**
+ * Converts an HSL color value to RGB. Conversion formula
+ * adapted from http://en.wikipedia.org/wiki/HSL_color_space.
+ * Assumes h, s, and l are contained in the set [0, 1] and
+ * returns r, g, and b in the set [0, 255].
+ *
+ * @param   Number  h       The hue
+ * @param   Number  s       The saturation
+ * @param   Number  l       The lightness
+ * @return  Array           The RGB representation
+ */
+function hslaToRgb(h, s, l, a){
+    var r, g, b;
+
+    if(s == 0){
+        r = g = b = l; // achromatic
+    }else{
+        function hue2rgb(p, q, t){
+            if(t < 0) t += 1;
+            if(t > 1) t -= 1;
+            if(t < 1/6) return p + (q - p) * 6 * t;
+            if(t < 1/2) return q;
+            if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+            return p;
+        }
+
+        var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+        var p = 2 * l - q;
+        r = hue2rgb(p, q, h + 1/3);
+        g = hue2rgb(p, q, h);
+        b = hue2rgb(p, q, h - 1/3);
+    }
+
+    return {r: Math.round(r * 255), g: Math.round(g * 255), b: Math.round(b * 255), a: a};
+}
+
+function cloneColor(color) {
+    return {r: color.r, g: color.g, b: color.b, a: color.a};
+}
+
+function colorVar(color, d) {
+    function rand(d) {
+        return d - (Math.random() * 2 * d);
+    }
+    return {r: color.r + rand(d), g: color.g + rand(d), b: color.b + rand(d), a: color.a};
+}
+
+function mixColors(col1, col2, pos) {
+    //Returns the color that's a mix between the 2 given ones, with position being the ratio between them
+    var nR, nG, nB, nA;
+    nR = Math.round(col1.r * (1 - pos) + col2.r * pos);
+    nG = Math.round(col1.g * (1 - pos) + col2.g * pos);
+    nB = Math.round(col1.b * (1 - pos) + col2.b * pos);
+    nA = col1.a * (1 - pos) + col2.a * pos;
+
+    return {col: {r: nR, g: nG, b: nB, a: nA},
+            string: "rgba(" + String(nR) + ", " + String(nG) + ", " + String(nB) + ", "  + String(nA) + ")"};
+}
